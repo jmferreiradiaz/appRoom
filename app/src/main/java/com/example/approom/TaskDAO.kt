0186c46abc66f904698164658bb50b97fa880f56
josem.ferreira@iesnervion.es <josem.ferreira@iesnervion.es>
2022-11-10 10:45:00 +0100

    dao

--- app/src/main/java/com/example/approom/TaskDAO.kt
+++ app/src/main/java/com/example/approom/TaskDAO.kt
@@ -6,11 +6,12 @@ import androidx.room.*
 interface TaskDao {
     @Query("SELECT * FROM task_entity")
     suspend fun getAllTasks(): MutableList<TaskEntity>
+
     @Query("SELECT * FROM task_entity WHERE id=:id")
-    suspend fun getTaskById(id:Int): TaskEntity
+    suspend fun getTaskById(id:Long): TaskEntity
 
     @Insert
-    suspend fun addTask(taskEntity : TaskEntity):Int
+    suspend fun addTask(taskEntity : TaskEntity):Long
 
     @Update
     suspend fun updateTask(task: TaskEntity)
